c5de4e78b49675bddc15bcc6a586fb828a899189
Naman Jain <jain96690@gmail.com>
2023-11-30 13:47:51 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import Sdata from './SData';
+function App(){
+  let MINUTE_MS = 10000;
+  let [data,setData]=useState([]);
+  let [c,setC]=useState(0);
+  const fetchApi= async ()=>{
+    if(c<Sdata.length){
+    console.log(Sdata[c]);
+       data.push(Sdata[c]);
+       setData(data);
+       setC(c++);
+    }
+   }
+    useEffect(()=> {
+      const interval =setInterval(fetchApi,MINUTE_MS);
+      return () => clearInterval(interval);
+    },[]);
+  return (<>    <h1 className='my-3 text-center'>Table</h1>
+     <div className='container my-5 shadow-lg text-center pb-1 pt-3 px-3' style={{backgroundColor:'#25d366'}}>
+       <table className="table table-bordered table-hover">
+  <thead>
+    <tr>
+      <th scope="col">S No</th>
+      <th scope="col">Temperature °C</th>
+      <th scope="col">Battery Level%</th>
+      <th scope="col">Weight</th>
+      <th scope="col">Door Lock/Unlock</th>
+      <th scope="col">Error Code</th>
+      <th scope="col">Date And Time</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      data && data.map((val,index)=>{
+        return(
+        <tr key={index}>
+        <td scope="row">{index+1}</td>
+        <td>{val.Temp}°C</td>
+        <td>{val.Battery}%</td>
+        <td>{val.Weight}kg</td>
+        <td>{val.Door}</td>
+        <td>{val.Error}</td>
+        <td>{val.Date}</td>
+      </tr> )
+      })
+    }    
 
-function App() {
-  return (
-    <h1>hello</h1>
-  );
+  </tbody>
+</table>
+       </div>
+       </>
+);
 }
 
 export default App;
---
+++ src/SData.jsx
@@ -0,0 +1,59 @@
+const Sdata=[
+    {
+     Temp:18,
+     Battery:91,
+     Weight:29,
+     Door:"Locked",
+     Error:"E-01",
+     Date:"10-03-2022 18:40:00"
+    },
+    {
+        Temp:17.5,
+        Battery:91,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 18:50:00"
+    },
+       {
+        Temp:17,
+        Battery:90,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 19:00:00"
+       },
+       {
+        Temp:16.5,
+        Battery:90,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 19:10:00"
+       },
+       {
+        Temp:16,
+        Battery:90,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 19:20:00"
+       },
+       {
+        Temp:15.5,
+        Battery:90,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 19:30:00"
+       },
+       {
+        Temp:15.1,
+        Battery:90,
+        Weight:29,
+        Door:"Locked",
+        Error:"E-01",
+        Date:"10-03-2022 19:40:00"
+       }
+];
+export default Sdata;
